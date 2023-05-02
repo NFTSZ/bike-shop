41c701d0b87ce2213e897e5d2848cf52f10b60f2
Naftaly Souza <naftsz.dev@gmail.com>
2023-05-02 18:19:37 -0300

    bug corrigido na funçao consultaPeca()

--- main.py
+++ main.py
@@ -20,26 +20,49 @@
         
 # inicio da função cadastrarPeca()
 def cadastrarPeca():
-    global codigo
-    while True:
-      codigo += 1
-      peca['Codigo'] = codigo
-      print(f'Código da peça: {codigo:03d}') # saída: 001
-      peca['Nome'] = str(input('Por favor, entre com o NOME da peça: '))
-      peca['Fabricante:'] = str(input('Por favor, entre com o FABRICANTE da peça: '))
-      peca['Valor'] = float(input('Por favor, entre com o VALOR da peça: '))
-      pecas.append(peca.copy())
-      break
+  global codigo
+  while True:
+    peca = {}
+    codigo += 1
+    peca['Codigo'] = codigo
+    print(f'Código da peça: {codigo:03d}') # saída: 001
+    peca['Nome'] = str(input('Por favor, entre com o NOME da peça: '))
+    peca['Fabricante'] = str(input('Por favor, entre com o FABRICANTE da peça: '))
+    peca['Valor'] = float(input('Por favor, entre com o VALOR da peça: '))
+    pecas.append(peca)
+    break
 # fim da função cadastrarPeca()
 
 # inicio da função consultaPeca()
 def consultaPeca():
-    print('')
+  while True:
+    print('Escolha a opção desejada:')
+    print('1 - Consultar todas as peças')
+    print('2 - Consultar peça por Código')
+    print('3 - Consultar peça por Fabricante')
+    print('4 - Retornar')
+    opcUser = int(input('>>> '))
+    if opcUser == 1:
+      for e in pecas:
+        print('--'*10)
+        for i,j in e.items():
+          print(f'{i}: {j}')
+        print('--'*10)
+    elif opcUser == 2:
+      codigoProduto = int(input('Digite o CÓDIGO da peça: ')) 
+      for peca in pecas:
+        if peca['Codigo'] == codigoProduto:
+          for i,j in peca.items():
+            print(f'{i}: {j}')          
+    elif opcUser == 4:
+      break
+      
+      
 # fim da função consultaPeca()
 
 # inicio da função removerPeca()
 def removerPeca():
-    print('')
+  print('')
 # fim da função removerPeca() 
 
 # inicio do main
@@ -55,10 +78,13 @@ while True:
   print('4 - Sair')
   opUser = int(input('>>> '))
   if opUser == 1:
+    print('Voce selecionou a opção de Cadastrar Peças')
     cadastrarPeca()
   elif opUser == 2:
+    print('Voce selecionou a opção de Consultar Peças')
     consultaPeca()
   elif opUser == 3:
+    print('Voce selecionou a opção de Remover Peças')
     removerPeca()
   elif opUser == 4:
     break
