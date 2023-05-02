73b1e50d699e579fed9862e3801de35ce80b38f5
Naftaly Souza <naftsz.dev@gmail.com>
2023-05-02 17:15:50 -0300

    função cadastrarPeca() completa

--- main.py
+++ main.py
@@ -7,22 +7,29 @@
         4) retornar
       3 - remover peças
       4 - SAIR
-      
-        for peca, valor in pecas.items():
-        print(peca, valor)'''
-      
+              
+      for e in pecas:
+        for i,j in e.items():
+          print(f'{i}: {j}')
+          
+      encerrar = int(input('>>'))
+      if encerrar == 1:
+        break
+      else:
+        continue'''
+        
 # inicio da função cadastrarPeca()
 def cadastrarPeca():
     global codigo
-    codigo += 1
-    print(f'Código da peça: 00{codigo}')
-    nomePeca = str(input('Por favor, entre com o NOME da peça: '))
-    fabricantePeca = str(input('Por favor, entre com o FABRICANTE da peça: '))
-    valorPeca = float(input('Por favor, entre com o VALOR da peça: '))
-    
-    pecas = {'Código': codigo, 'Nome:': nomePeca, 'Fabricante:': fabricantePeca, 'Valor:': valorPeca}
- 
-    
+    while True:
+      codigo += 1
+      peca['Codigo'] = '{:03d}'.format(codigo)
+      print(f'Código da peça: {codigo:03d}') # saída: 001
+      peca['Nome'] = str(input('Por favor, entre com o NOME da peça: '))
+      peca['Fabricante:'] = str(input('Por favor, entre com o FABRICANTE da peça: '))
+      peca['Valor'] = float(input('Por favor, entre com o VALOR da peça: '))
+      pecas.append(peca.copy())
+      break
 # fim da função cadastrarPeca()
 
 # inicio da função consultaPeca()
@@ -37,7 +44,18 @@ def removerPeca():
 
 # inicio do main
 codigo = 0
+pecas = []
+peca = {}
 print('Bem vindo(a) ao Controle de Estoque da Bicicletaria da Naftaly Benedita Souza')
-
-cadastrarPeca()
+while True:
+  print('Escolha a opção desejada:')
+  print('1 - Cadastrar peças')
+  print('2 - Consultar Peças')
+  print('3 - remover peças')
+  print('4 - SAIR')
+  opUser = int(input('>>> '))
+  if opUser == 1:
+    cadastrarPeca()
+  elif opUser == 2:
+    consultaPeca()
 # fim do main
